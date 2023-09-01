7e948de4ebe393c9e62b493b2626226159b2182a
rrenat358 <rrenat358@gmail.com>
2023-09-01 17:38:10 +0300

    1737

---
+++ PhoneBook/core-service/src/main/java/ru/rrenat358/core/PhoneBookApp.java
@@ -0,0 +1,17 @@
+package ru.rrenat358.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class PhoneBookApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PhoneBookApp.class, args);
+
+    }
+
+
+}
