7163fd2bc9942eb9e5002aa059c47f572fde6108
rrenat358 <rrenat358@gmail.com>
2023-09-15 17:42:31 +0300

    1741 Front - bugfix create newClient for clientName=""

--- PhoneBook/front-service/src/main/resources/static/new-client/new-client.js
+++ PhoneBook/front-service/src/main/resources/static/new-client/new-client.js
@@ -1,23 +1,18 @@
 angular.module('phonebook-front').controller('newClientController', function ($scope, $http) {
     const contextPath = 'http://localhost:5555/core/api/v1';
 
-    // $scope.newClient = "";
     $scope.newClient = undefined;
 
     $scope.saveNewClient = function () {
-        if ($scope.newClient.name === "") {
+        if ($scope.newClient === undefined || $scope.newClient.name === "") {
             return;
         }
         $http.post(contextPath + '/clients', $scope.newClient)
             .then(function (response) {
                 $scope.loadClient = response.data;
-                // $scope.newClient = "";
+                $scope.newClient.name = "";
             });
     }
-
-
-
-
 
 
 
