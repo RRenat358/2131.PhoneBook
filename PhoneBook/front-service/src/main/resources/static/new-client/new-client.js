e570fef73191bafbfd22a3a64473b1e88baf0d55
rrenat358 <rrenat358@gmail.com>
2023-09-15 17:27:53 +0300

    1727

--- PhoneBook/front-service/src/main/resources/static/new-client/new-client.js
+++ PhoneBook/front-service/src/main/resources/static/new-client/new-client.js
@@ -1,13 +1,24 @@
 angular.module('phonebook-front').controller('newClientController', function ($scope, $http) {
     const contextPath = 'http://localhost:5555/core/api/v1';
 
+    // $scope.newClient = "";
+    $scope.newClient = undefined;
 
     $scope.saveNewClient = function () {
+        if ($scope.newClient.name === "") {
+            return;
+        }
         $http.post(contextPath + '/clients', $scope.newClient)
             .then(function (response) {
                 $scope.loadClient = response.data;
+                // $scope.newClient = "";
             });
     }
+
+
+
+
+
 
 
 });
