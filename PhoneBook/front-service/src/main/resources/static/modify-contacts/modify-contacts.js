42efc8b61a7860134f50cb30d5c4b3afcfe55362
rrenat358 <rrenat358@gmail.com>
2023-09-18 22:59:28 +0300

    2258 clear sendClientId

--- PhoneBook/front-service/src/main/resources/static/modify-contacts/modify-contacts.js
+++ PhoneBook/front-service/src/main/resources/static/modify-contacts/modify-contacts.js
@@ -14,6 +14,7 @@ angular.module('phonebook-front').controller('modifyContactsController', functio
                     $scope.clientName = response.data.name;
                     $scope.loadAllEmailByClientId();
                     $scope.loadAllPhoneByClientId();
+                    $scope.sendClientId = "";
                 });
         }
     }
